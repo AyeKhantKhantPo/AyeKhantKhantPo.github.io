a4532e765861ebcb7c0e12c2c5de557b9d9fac5d
ayekhantkhantpo <ayekhantkhantpo@gmail.com>
2024-10-10 17:00:32 +0630

    add skill and experience part

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 
-import styles from "./About.module.css"
+import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 export const About = () => {
   return (
-    <section className={styles.container}>
-      <h2>About</h2>
-      <div>
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
         <img
           src={getImageUrl("about/aboutImage.png")}
           alt="Me sitting with a laptop"
+          className={styles.aboutImage}
         />
-        <ul>
-          <li>
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-            <div>
+            <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
                 I'm a frontend developer with experience in building responsive
@@ -23,9 +24,9 @@ export const About = () => {
               </p>
             </div>
           </li>
-          <li>
+          <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-            <div>
+            <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
               <p>
                 I have experience developing fast and optimised backend systems
@@ -33,12 +34,13 @@ export const About = () => {
               </p>
             </div>
           </li>
-          <li>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-            <div>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
               <p>
-                I have designed multiple landing pages and have created design system as well
+                I have designed multiple landing pages and have created design
+                system as well
               </p>
             </div>
           </li>
